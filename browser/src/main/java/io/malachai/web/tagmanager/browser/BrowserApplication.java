6a9581e1eb941c468c5bed451deacacaed404485
Malachai <xogmldntmd@naver.com>
2023-10-13 15:47:54 +0900

    Feat: Dummy DumpController prints dumps

--- browser/src/main/java/io/malachai/web/tagmanager/browser/BrowserApplication.java
+++ browser/src/main/java/io/malachai/web/tagmanager/browser/BrowserApplication.java
@@ -2,8 +2,10 @@ package io.malachai.web.tagmanager.browser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BrowserApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrowserApplication.class, args);
---
+++ browser/src/main/java/io/malachai/web/tagmanager/browser/config/BrowserConfig.java
@@ -0,0 +1,8 @@
+package io.malachai.web.tagmanager.browser.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BrowserConfig {
+
+}
